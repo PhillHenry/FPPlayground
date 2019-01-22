f707124c683184ccd776d44e1744a1f82e06a4c0
PhillHenry <phillhenry@gmail.com>
2019-01-22 09:53:17 +0000

    Most descriptive yet.

--- src/main/scala/uk/co/odinconsultants/fp/scalaz/Monads.scala
+++ src/main/scala/uk/co/odinconsultants/fp/scalaz/Monads.scala
@@ -20,11 +20,13 @@ object MonadX {
   def apply[A](f: Context => A, name: String): M[A] = new M[A] {
     override def run(ctx: Context): A = {
       //println((new Exception()).getStackTrace.drop(1).take(15).map("\t" + _).mkString("\n"))
-      log(s"$this.run ...")
+      log(s"$this.run")
       depth = depth + 1
-      //log(s"calling $this.f(ctx) where f = $f")
+      log(s"$f(ctx) = ")
+      depth = depth + 1
       val fResult = f(ctx)
-      log(s"$this.f(ctx) = $fResult")
+      log(s"'$fResult'")
+      depth = depth - 1
       depth = depth - 1
       log(s"$this.run Finished")
       fResult
@@ -34,21 +36,26 @@ object MonadX {
 
   implicit val monad = new Monad[MonadX] {
     override def bind[A, B](fa: M[A])(f: A ⇒ M[B]): M[B] = {
-      log(s"Binding $fa, f=$f ...")
-      MonadX(newF(fa, f), "bound" + fa.toString)
+      log(s"bind: Creating bound$fa with fa=$fa, f=$f")
+      val name = "bound" + fa.toString
+      MonadX(newF(fa, f, name), name)
     }
     override def point[A](a: ⇒ A): M[A] = {
       log(s"Creating point ($a) [${a.getClass.getSimpleName}]")
       MonadX(_ ⇒ a, "point")
     }
-    def newF[B, A](fa: M[A], f: A => M[B]): Context => B = { ctx ⇒
-      val faRan:    A     = fa.run(ctx)
-      log(s"calling f($faRan) where f = $f")
-      depth = depth + 1
-      val fResult:  M[B]  = f(faRan)
-      depth = depth - 1
-      log(s"f($faRan) = $fResult")
-      fResult.run(ctx)
+    def newF[B, A](fa: M[A], f: A => M[B], name: String): Context => B = new Function1[Context, B] {
+      def apply(ctx: Context) = {
+        val faRan:    A     = fa.run(ctx)
+        log(s"$f($faRan) = ")
+        depth = depth + 1
+        val fResult:  M[B]  = f(faRan)
+        log(s"'$fResult'")
+        depth = depth - 1
+        fResult.run(ctx)
+      }
+
+      override def toString(): String = s"$name.f"
     }
   }
 }
@@ -56,33 +63,41 @@ object MonadX {
 object Monads {
   import scalaz.Scalaz._
   def main(args: Array[String]): Unit = {
-    val hello:    MonadX[String]  = MonadX(helloFn,     "hello")
-    val hashcode: MonadX[Long]    = MonadX(hashCodeFn,  "hashCode")
-/*Binding hello ...                                                 # boundhello is created with { ctx => ... f = { x <- hello }; fa = hello ... }, but nothing further called
+    val hello:    MonadX[String]  = MonadX(helloFn,           "Hello")
+    val hashcode: MonadX[Long]    = MonadX(meaningOfLifeFn,   "MeaningOfLife")
+/*
+About to run for-comprehension
+==============================
+bind: Creating boundHello with fa=Hello, f=<function1>
 
 About to run boundhello
 =======================
-boundhello.run ...
-    hello.run ...
-        hello.f(ctx) = bonjour
-    hello.run Finished
-    calling f(bonjour) where f = <function1>
-        Binding hashCode, f=<function1> ...
-    f(bonjour) = boundhashCode
-    boundhashCode.run ...
-        hashCode.run ...
-            hashCode.f(ctx) = -1184959538
-        hashCode.run Finished
-        calling f(-1184959538) where f = <function1>
-            Creating point (18) [Integer]
-        f(-1184959538) = point
-        point.run ...
-            point.f(ctx) = 18
-        point.run Finished
-        boundhashCode.f(ctx) = 18
-    boundhashCode.run Finished
-    boundhello.f(ctx) = 18
-boundhello.run Finished
+boundHello.run
+    boundHello.f(ctx) =
+        Hello.run
+            helloFn(ctx) =
+                'hello'
+        Hello.run Finished
+        <function1>(hello) =
+            bind: Creating boundMeaningOfLife with fa=MeaningOfLife, f=<function1>
+            'boundMeaningOfLife'
+        boundMeaningOfLife.run
+            boundMeaningOfLife.f(ctx) =
+                MeaningOfLife.run
+                    meaningOfLifeFn(ctx) =
+                        '42'
+                MeaningOfLife.run Finished
+                <function1>(42) =
+                    Creating point (7) [Integer]
+                    'point'
+                point.run
+                    <function1>(ctx) =
+                        '7'
+                point.run Finished
+                '7'
+        boundMeaningOfLife.run Finished
+        '7'
+boundHello.run Finished
 
 * See Monad.map which says: map[A,B](fa: F[A])(f: A => B): F[B] = bind(fa)(a => point(f(a)))
      */
@@ -97,7 +112,7 @@ boundhello.run Finished
       (x + y).length
     }
 
-    val map = Map("hello" -> "bonjour")
+    val map = Context("hello", 42)
     println(underline(s"About to run $boundhello"))
     val helloHash = boundhello.run(map)
     println()
@@ -105,14 +120,14 @@ boundhello.run Finished
     println(helloHash) // "18"
   }
   val helloFn = new Function1[Context, String] {
-    override def apply(ctx: Context): String = ctx("hello")
+    override def apply(ctx: Context): String = ctx.aString
     override def toString():          String = "helloFn"
   }
-  val hashCodeFn = new Function1[Context, Long] {
-    override def apply(ctx: Context): Long    = ctx.hashCode
-    override def toString():          String  = "hashCodeFn"
+  val meaningOfLifeFn = new Function1[Context, Long] {
+    override def apply(ctx: Context): Long    = ctx.aLong
+    override def toString():          String  = "meaningOfLifeFn"
   }
-  type Context = Map[String, String]
+  case class Context(aString: String, aLong: Long)
 
   def underline(x: String): String = "\n" + x + "\n" + ("=" * x.length)
 

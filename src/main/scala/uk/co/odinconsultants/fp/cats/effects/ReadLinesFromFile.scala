0cc726d054f8cf627a19f0d2d493f90a2c9ca775
PhillHenry <Creek1234>
2019-01-31 05:58:38 +0000

    scala.io.Source is not autoclosable.

--- src/main/scala/uk/co/odinconsultants/fp/cats/effects/ReadLinesFromFile.scala
+++ src/main/scala/uk/co/odinconsultants/fp/cats/effects/ReadLinesFromFile.scala
@@ -1,25 +1,26 @@
 package uk.co.odinconsultants.fp.cats.effects
 
-import java.io.File
+import java.io.{BufferedReader, File, FileReader}
 
 import cats.effect._
 import cats.implicits._
 
-import scala.io.Source
-
 /**
-  * From Fabio Labella on Gitter
+  * Adapted rom Fabio Labella on Gitter
   */
 object ReadLinesFromFile {
 
   def addPlus(x: String): String = x + "+"
 
+  def readLines(src: BufferedReader): List[String] =
+    src.lines().toArray().toList.map(_.toString)
+
   def readFile[F[_]: Sync](file: File): F[List[String]] =
     Resource.fromAutoCloseable{
-      Sync[F].delay(Source.fromFile(file))
+      Sync[F].delay(new BufferedReader(new FileReader(file)))
     }.use { src =>
       Sync[F]
-        .delay(src.getLines)
-        .map(_.toList.map(addPlus))
+        .delay(readLines(src))
+        .map(_.map(x => addPlus(x)))
     }
 }

9f00af7d562802214b6156fc8c5577314190e7c5
PhillHenry <phillhenry@gmail.com>
2019-02-18 17:06:21 +0000

    Slightly changed from Gabriel Volpe's (gvolpe) code.

---
+++ src/main/scala/uk/co/odinconsultants/fp/cats/state/FooAlg.scala
@@ -0,0 +1,28 @@
+package uk.co.odinconsultants.fp.cats.state
+
+import cats._
+import cats.implicits._
+//import cats.effect.{IO, Timer}
+import cats.effect._
+import cats.effect.concurrent.Ref
+
+
+case class Foo(x: String)
+
+trait FooAlg[F[_]] {
+  def get: F[Foo]  // translate into business term
+  def set(foo: Foo): F[Unit]
+}
+
+object FooAlg {
+
+  def main(args: Array[String]): Unit = { println("hello") }
+
+  def create[F[_]: Sync]: F[FooAlg[F]] =
+    Ref.of[F, Foo](Foo("test")).map { ref =>
+      new FooAlg[F] {
+        def get: F[Foo] = ref.get
+        def set(foo: Foo): F[Unit] = ref.set(foo)
+      }
+    }
+}

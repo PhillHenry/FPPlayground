fc8a1f348c2447a026f02a9f5ab34c6976050849
PhillHenry <phillhenry@gmail.com>
2019-02-23 10:26:49 +0000

    Trying to mix computation with logging.

--- src/main/scala/uk/co/odinconsultants/fp/cats/io/MyIO.scala
+++ src/main/scala/uk/co/odinconsultants/fp/cats/io/MyIO.scala
@@ -1,20 +1,22 @@
 package uk.co.odinconsultants.fp.cats.io
 
 import cats.effect.IO
-import uk.co.odinconsultants.fp.cats.io.MyIO.putStrlLn
+import cats.implicits._
 
 /**
   * @see https://typelevel.org/cats-effect/datatypes/io.html
   */
 object MyIO extends App {
 
-  def fib(n: Int, a: Long = 0, b: Long = 1): IO[Long] =
+  def fib(n: Int, a: Long = 0, b: Long = 1): IO[Long] = {
+//    println(s"fib = $n")
     IO(a + b).flatMap { b2 =>
       if (n > 0)
         fib(n - 1, b, b2)
       else
         IO.pure(b2)
     }
+  }
 
   def putStrlLn(value: String) = IO(println(value))
   val readLn = IO(scala.io.StdIn.readLine)
@@ -28,11 +30,12 @@ object MyIO extends App {
 
 
   val fibIO = for {
-    _ <- putStrlLn("Starting fibonacci ")
-    x <- fib(10)
-    _ <- putStrlLn(x.toString) // x is a Long
-  } yield ()
-  fibIO.unsafeRunSync()
+    _   <- putStrlLn("Starting fibonacci ")
+    is  <- IO { List(1,2,3) }
+    xs  <- IO { is.map(i => fib(i)) }
+    r   <- xs.map(x => putStrlLn(x.toString)).sequence // x is a Long
+  } yield r
+  fibIO.unsafeRunSync() // hmm, this just prints out the IOs
 
 
   val ioa = IO { println("hey!") }

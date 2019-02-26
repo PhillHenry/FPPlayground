3ca935e2efe9dfc79580da5e8337e3c0357616c9
PhillHenry <phillhenry@gmail.com>
2019-02-26 17:50:29 +0000

    Upgraded cats and the Scala compiler

---
+++ src/main/scala/uk/co/odinconsultants/fp/cats/fs2/ZippingStreams.scala
@@ -0,0 +1,22 @@
+package uk.co.odinconsultants.fp.cats.fs2
+
+import cats.implicits._
+import cats.effect.{ExitCode, IO, IOApp}
+import fs2.Stream
+
+import scala.concurrent.duration._
+
+object Zipping extends IOApp {
+  override def run(args: List[String]): IO[ExitCode] = {
+    val nums  = Stream.range[IO](1, 10).metered(25.millis)
+    val chars = Stream.range[IO]('a', 'z' + 1).map(_.toChar).metered(10.millis)
+
+    val zipped = for {
+      a <- nums.noneTerminate.hold(1L.some)
+      b <- chars.noneTerminate.hold('\0'.some)
+      c <- (a, b).mapN(_ product _).discrete.unNoneTerminate
+    } yield c
+
+    zipped.evalTap(el => IO { println(el) }).compile.drain.as(ExitCode.Success)
+  }
+}

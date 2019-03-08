1574c0d41370c04d830423bd7cd7f03fd3a66dd9
PhillHenry <phillhenry@gmail.com>
2019-03-08 15:50:04 +0000

    Added the latest shapeless.

---
+++ src/main/scala/uk/co/odinconsultants/fp/shapeless/numerical/NumericalMain.scala
@@ -0,0 +1,11 @@
+package uk.co.odinconsultants.fp.shapeless.numerical
+
+import shapeless.syntax.singleton._
+
+object NumericalMain {
+
+  def main(args: Array[String]): Unit = {
+    println(42.narrow)
+  }
+
+}

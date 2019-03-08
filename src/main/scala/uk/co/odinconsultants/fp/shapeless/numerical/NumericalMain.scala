c89465a6fb6141b2990cd824e77b66cba01ed887
PhillHenry <phillhenry@gmail.com>
2019-03-08 16:05:56 +0000

    More shapeless fun.

--- src/main/scala/uk/co/odinconsultants/fp/shapeless/numerical/NumericalMain.scala
+++ src/main/scala/uk/co/odinconsultants/fp/shapeless/numerical/NumericalMain.scala
@@ -1,11 +1,36 @@
 package uk.co.odinconsultants.fp.shapeless.numerical
 
+import shapeless.Witness
 import shapeless.syntax.singleton._
 
 object NumericalMain {
 
+  class SillyMatrix[U, V] {}
+
+//  type T = Witness(42.narrow)
+  val (wTrue, wFalse) = (Witness(true), Witness(false))
+
   def main(args: Array[String]): Unit = {
+    val w0 = Witness(0)
+    type _0 = w0.T
+    val w42 = Witness(42)
+    type _42 = w42.T
+    val w18 = Witness(18)
+    type _18 = w18.T
+    val w3 = Witness(3)
+    type _3 = w3.T
+
+    val a = new SillyMatrix[_42, _18]
+    val b = new SillyMatrix[_18, _3]
+    val c = new SillyMatrix[_0, _3]
     println(42.narrow)
+
+    sillyMatrixMultiply(a, b)
+//    sillyMatrixMultiply(a, c) // this doesn't compile - as indeed should be the case!
+  }
+
+  def sillyMatrixMultiply[X, Y, Z](a: SillyMatrix[X, Y], b: SillyMatrix[Y, Z]) = {
+    println("Everything appears fine")
   }
 
 }

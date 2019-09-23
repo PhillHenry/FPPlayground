947711caf36922469052bb4ff752e8f6c59ecd7e
HenryP <philip.henry@vodafone.com>
2019-09-23 09:32:25 +0100

    Test.

--- src/test/scala/uk/co/odinconsultants/fp/cats/validation/ValidatorSpec.scala
+++ src/test/scala/uk/co/odinconsultants/fp/cats/validation/ValidatorSpec.scala
@@ -1,14 +1,23 @@
 package uk.co.odinconsultants.fp.cats.validation
 
+import cats.implicits._
 import org.scalatest.{Matchers, WordSpec}
 
 class ValidatorSpec extends WordSpec with Matchers {
 
-  import Validator._
+  val first     = "Hello"
+  val second    = "World"
+  val third     = "(the end)"
+  val expected  = s"$first $second $third"
 
   "Options" should {
-    "return something for teh happy path" in {
 
+    "return something for the happy path" in {
+      val TW = Option(first)
+      val UW = Option(second)
+      val VW = Option(third)
+      val x = new Validator(TW, UW, VW)
+      x.process shouldBe Some(expected)
     }
   }
 

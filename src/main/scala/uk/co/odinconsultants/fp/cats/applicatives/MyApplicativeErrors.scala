75888a0f08fdb67f3c87bf955e8c155f2cf3739a
PhillHenry <phillhenry@gmail.com>
2019-10-18 15:30:49 +0100

    Created. Not quite sure why it sometimes doesn't compile (imports?).

---
+++ src/main/scala/uk/co/odinconsultants/fp/cats/applicatives/MyApplicativeErrors.scala
@@ -0,0 +1,39 @@
+package uk.co.odinconsultants.fp.cats.applicatives
+
+import cats.data.NonEmptyList
+import cats.{Applicative, ApplicativeError}
+import java.io.{InputStream, OutputStream}
+
+import cats.effect.{ExitCode, IO, IOApp}
+
+import scala.util.Try
+
+class MyApplicativeErrors[F[_]: Applicative](implicit E: ApplicativeError[F, Throwable]) {
+
+  def doIO[U](f: => U): F[U] = E.fromTry(Try(f))
+
+  def allOrNothing[A](xs: NonEmptyList[F[A]]): F[A] = {
+    import cats.implicits._
+    xs.foldLeft(xs.head) { case (a, x) =>
+      a *> x
+    }
+  }
+
+  def doIO(firstOutputStream: OutputStream, secondOutputString: OutputStream, inputStream: InputStream): F[Unit] = {
+    val fo1 = doIO(firstOutputStream.close())
+    val fo2 = doIO(secondOutputString.close())
+    val fi = doIO(inputStream.close())
+
+    allOrNothing(NonEmptyList(fo1, List(fo2, fi)))
+  }
+}
+
+object MyApplicativeErrors {
+
+  def main(args: Array[String]): Unit = {
+    type MyType[T] = Either[Throwable, T]
+    import cats.implicits._
+    val x = new MyApplicativeErrors[MyType]
+    println(x)
+  }
+}

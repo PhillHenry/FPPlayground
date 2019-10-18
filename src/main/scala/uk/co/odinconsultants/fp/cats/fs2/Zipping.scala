ba689b6db32bcd8231fe7d3c940d9493e65bc372
HenryP <philip.henry@vodafone.com>
2019-10-18 14:16:59 +0100

    Playing around with streams and zippling.

--- src/main/scala/uk/co/odinconsultants/fp/cats/fs2/Zipping.scala
+++ src/main/scala/uk/co/odinconsultants/fp/cats/fs2/Zipping.scala
@@ -8,17 +8,42 @@ import scala.concurrent.duration._
 
 object Zipping extends IOApp {
   override def run(args: List[String]): IO[ExitCode] = {
-    val nums  = Stream.range[IO](1, 10).metered(25.millis)
-    val chars = Stream.range[IO]('a', 'z' + 1).map(_.toChar).metered(10.millis)
+    val nums  = Stream.range[IO](1, 10)
+    val chars = Stream.range[IO]('a', 'z' + 1).map(_.toChar)
 
+    zipWithHold(nums, chars)
+
+    flatMapOver(nums, chars)
+  }
+
+  private def flatMapOver(nums: Stream[IO, Int], chars: Stream[IO, Char]) = {
+    val flatMapped = nums.zip(chars).flatMap { case (a, b) =>
+      val x = s"a = $a, b = $b"
+      Stream.emit(x)
+    }
+    printOut(flatMapped)
+  }
+
+  private def zipWithHold(x: Stream[IO, Int], y: Stream[IO, Char]): IO[ExitCode] = {
+    val nums  = x.metered(25.millis)
+    val chars = y.metered(10.millis)
     val zipped = for {
       a <- nums.noneTerminate.hold(1L.some)
       b <- chars.noneTerminate.hold('\0'.some)
-      c <-  {(a, b).mapN(_ product _)
-        .discrete
-        .unNoneTerminate} // product is actually FlatMap.product
+      c <- {
+        (a, b).mapN(_ product _)
+          .discrete
+          .unNoneTerminate
+      } // product is actually FlatMap.product
     } yield c
 
-    zipped.evalTap(el => IO { println(el) }).compile.drain.as(ExitCode.Success)
+    printOut(zipped)
+  }
+
+
+  private def printOut[T](zipped: Stream[IO, T]): IO[ExitCode] = {
+    zipped.evalTap(el => IO {
+      println(el)
+    }).compile.drain.as(ExitCode.Success)
   }
 }

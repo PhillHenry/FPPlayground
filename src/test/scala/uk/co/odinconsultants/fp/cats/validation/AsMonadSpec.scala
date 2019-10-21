8ccbd1f1b739c3af7cf68decd54e9ced54ce9004
HenryP <philip.henry@vodafone.com>
2019-10-21 13:27:30 +0100

    EitherNel.

--- src/test/scala/uk/co/odinconsultants/fp/cats/validation/AsMonadSpec.scala
+++ src/test/scala/uk/co/odinconsultants/fp/cats/validation/AsMonadSpec.scala
@@ -23,5 +23,32 @@ class AsMonadSpec extends WordSpec with Matchers {
       result shouldBe valid2
     }
   }
+  "EitherNel" should {
+    import cats.data.EitherNel
+    type MyErrorType    = Throwable
+    type MyEitherNel[T] = EitherNel[MyErrorType, T]
+    type MyDataType     = MyEitherNel[String]
+
+    val good: MyDataType = Right("success!")
+    val bad:  MyDataType = Left(NonEmptyList(new Throwable("failure"), List()))
+    val mixedList: NonEmptyList[MyDataType] = NonEmptyList(good, List(good, bad))
+
+    "be applicable" in {
+      import cats.implicits._
+//      val x = new AsMonad[MyEitherNel, MyErrorType]
+//      x.allOrNothing(mixedList) shouldBe bad
+    }
+    "be flatmappable" in {
+      import cats.implicits._
+//      val x = new AsMonad[MyEitherNel, MyErrorType]
+//      val allGood = new NonEmptyList(good, List(good))
+//      val result = for {
+//        y <- x.allOrNothing(allGood)
+//        z <- x.allOrNothing(allGood)
+//      } yield z
+//      println(result)
+//      result shouldBe good
+    }
+  }
 
 }
--- src/test/scala/uk/co/odinconsultants/fp/cats/validation/ValidatedAsApplicativeSpec.scala
+++ src/test/scala/uk/co/odinconsultants/fp/cats/validation/ValidatedAsApplicativeSpec.scala
@@ -1,7 +1,7 @@
 package uk.co.odinconsultants.fp.cats.validation
 
 import cats.{Applicative, ApplicativeError}
-import cats.data.{NonEmptyChain, Validated, ValidatedNec}
+import cats.data.{NonEmptyChain, NonEmptyList, Validated, ValidatedNec}
 import cats.data.Validated.{Invalid, Valid}
 import org.scalatest.{Matchers, WordSpec}
 
@@ -11,6 +11,40 @@ class ValidatedAsApplicativeSpec extends WordSpec with Matchers {
     def create[F[_]: Applicative, X](implicit E: ApplicativeError[F, X]): ValidatedAsApplicative[F, X] = {
       println(s"E = $E")
       new ValidatedAsApplicative[F, X]
+    }
+  }
+
+
+  trait EitherNecApplicativeFixture extends CreationFixture {
+
+    type MyErrorType    = String
+    type MyDataType     = String
+    type MyX            = NonEmptyList[MyErrorType]
+    type MyAppError[T]  = Either[MyX, T]
+
+    def underTest: ValidatedAsApplicative[MyAppError, MyX] = {
+      import cats.implicits._
+      create[MyAppError, MyX]
+    }
+
+    def myFailure(x: String): MyAppError[MyDataType] = Left(NonEmptyList(x, List.empty))
+
+    val valid1: MyAppError[MyDataType] = Right("valid1")
+    val valid2: MyAppError[MyDataType] = Right("valid2")
+    val invalid1Msg = "invalid1Msg"
+    val invalid2Msg = "invalid2Msg"
+  }
+
+  "An applicative of EitherNel" should {
+    "have a happy path of Right" in new EitherNecApplicativeFixture {
+      val rightMsg = "a string"
+      val pure = underTest.pureHappyPath(rightMsg)
+      pure shouldBe Right(rightMsg)
+    }
+    "fail on the first Left" in new EitherNecApplicativeFixture {
+      val xs: NonEmptyList[MyAppError[String]] = NonEmptyList(valid1, List(myFailure(invalid1Msg), valid2, myFailure(invalid2Msg)))
+      val allValidated = underTest.allOrNothing(xs)
+      allValidated shouldBe myFailure(invalid1Msg)
     }
   }
 

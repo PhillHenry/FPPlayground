0522eaf499b601c7364f4307964990fe2d214fef
HenryP <philip.henry@vodafone.com>
2019-10-25 09:05:38 +0100

    Working accumulation.

--- src/test/scala/uk/co/odinconsultants/fp/cats/applicatives/MyParSpec.scala
+++ src/test/scala/uk/co/odinconsultants/fp/cats/applicatives/MyParSpec.scala
@@ -1,6 +1,6 @@
 package uk.co.odinconsultants.fp.cats.applicatives
 
-import cats.data.EitherNec
+import cats.data.{EitherNec, NonEmptyChain}
 import org.scalatest.{Matchers, WordSpec}
 import uk.co.odinconsultants.fp.cats.validation.{EitherFixture, ValidatedFixture}
 
@@ -8,17 +8,34 @@ class MyParSpec extends WordSpec with Matchers {
 
   import MyPar._
 
+  trait EitherStringFixture {
+    type MyEither = EitherNec[String, String]
+    val success1Msg = "success1!"
+    val success2Msg = "success2!"
+    val success3Msg = "success3!"
+    val valid1: MyEither = Right(success1Msg)
+    val valid2: MyEither = Right(success2Msg)
+    val valid3: MyEither = Right(success3Msg)
+
+    import cats.data._
+
+    val failure1Msg = "failure1"
+    val failure2Msg = "failure2"
+    val invalid1: MyEither = Left(NonEmptyChain(failure1Msg))
+    val invalid2: MyEither = Left(NonEmptyChain(failure2Msg))
+  }
+
   "par" should {
     "accumulate errors for Validated" in new ValidatedFixture {
       parToAccumulateErrorsInValidated(valid1, valid2, valid3)
     }
-    "accumulate errors for Either" in {
-      type MyEither = EitherNec[String, String]
-      val valid1: MyEither = Right("success!")
-      val valid2: MyEither = Right("success!")
-      val valid3: MyEither = Right("success!")
+    "accumulate success for Either" in new EitherStringFixture {
       val accumulated: EitherNecAcc = parToAccumulateErrorsInEitherNec(valid1, valid2, valid3)
-      println(accumulated)
+      accumulated shouldBe Right(concatStrings(success1Msg, success2Msg, success3Msg))
+    }
+    "accumulate errors" in new EitherStringFixture {
+      val accumulated: EitherNecAcc = parToAccumulateErrorsInEitherNec(valid1, invalid1, invalid2)
+      accumulated shouldBe Left(NonEmptyChain(failure1Msg, failure2Msg))
     }
   }
 

f17e0bb137ee70b23d06ba3fe4e4504650763b4e
PhillHenry <phillhenry@gmail.com>
2019-11-17 16:54:40 +0000

    Refactoring.

--- src/main/scala/uk/co/odinconsultants/fp/cats/fs2/example/ConsumerKafka.scala
+++ src/main/scala/uk/co/odinconsultants/fp/cats/fs2/example/ConsumerKafka.scala
@@ -8,27 +8,26 @@ import scala.concurrent.duration._
 
 object ConsumerKafka {
 
-  type MyPipe = Stream[IO, ProducerRecords[String, String, CommittableOffset[IO]]] => Stream[IO, ProducerResult[String, String, CommittableOffset[IO]]]
-
-  type BatchPipe = Pipe[IO, CommittableOffset[IO], Unit]
-
   import Settings._
 
-  type MyKafkaConsumer = KafkaConsumer[IO, String, String]
+  type MyKafkaConsumer  = KafkaConsumer[IO, String, String]
+  type MyProducer       = ProducerRecords[String, String, CommittableOffset[IO]]
+  type MyProducerResult = ProducerResult[String, String, CommittableOffset[IO]]
 
-  def processRecord(record: ConsumerRecord[String, String]): IO[(String, String)]
-  = IO.pure(record.key -> record.value)
+  type ProducerPipe     = Pipe[IO, MyProducer, MyProducerResult]
+  type BatchPipe        = Pipe[IO, CommittableOffset[IO], Unit]
 
-  val subscribeFn: MyKafkaConsumer => IO[Unit]
-  = _.subscribeTo(topicName)
+
+  def processRecord(record: ConsumerRecord[String, String]): IO[(String, String)] = IO.pure(record.key -> record.value)
+
+  val subscribeFn: MyKafkaConsumer => IO[Unit] = _.subscribeTo(topicName)
 
   /**
    * When using stream, records on all assigned partitions end up in the same Stream.
    */
-  val toStreamFn: MyKafkaConsumer => Stream[IO, CommittableConsumerRecord[IO, String, String]]
-  = _.stream
+  val toStreamFn: MyKafkaConsumer => Stream[IO, CommittableConsumerRecord[IO, String, String]] = _.stream
 
-  val commitFn: CommittableConsumerRecord[IO, String, String] => IO[ProducerRecords[String, String, CommittableOffset[IO]]] = { committable =>
+  val commitFn: CommittableConsumerRecord[IO, String, String] => IO[MyProducer] = { committable =>
     println(s"committable = $committable")
     val io: IO[(String, String)] = processRecord(committable.record)
     io.map { case (key, value) =>
@@ -38,15 +37,15 @@ object ConsumerKafka {
     }
   }
 
-  val passingThroughFn: ProducerResult[String, String, CommittableOffset[IO]] => CommittableOffset[IO] = _.passthrough
+  val passingThroughFn: MyProducerResult => CommittableOffset[IO] = _.passthrough
 
-  def kafkaConsumer(implicit io: ConcurrentEffect[IO], context: ContextShift[IO], timer: Timer[IO]): Stream[IO, KafkaConsumer[IO, String, String]] =
-    consumerStream[IO]
-      .using(consumerSettings)
+  def kafkaConsumer(implicit io: ConcurrentEffect[IO], context: ContextShift[IO], timer: Timer[IO]): Stream[IO, MyKafkaConsumer] =
+    consumerStream[IO].using(consumerSettings)
 
-  def producerPipe(implicit f: ConcurrentEffect[IO],  context: ContextShift[IO]): MyPipe =
+  def producerPipe(implicit f: ConcurrentEffect[IO],  context: ContextShift[IO]): ProducerPipe =
     fs2.kafka.produce(producerSettings)(f, context)
 
-  def committingBatch(implicit io: ConcurrentEffect[IO], timer: Timer[IO]): BatchPipe = fs2.kafka.commitBatchWithin(500, 15.seconds)
+  def committingBatch(implicit io: ConcurrentEffect[IO], timer: Timer[IO]): BatchPipe =
+    fs2.kafka.commitBatchWithin(500, 15.seconds)
 
 }

d36d26aa35693521ff9e9a5802ce3a7463becf9a
PhillHenry <phillhenry@gmail.com>
2019-11-26 05:27:45 +0000

    Nomenclature.

--- src/main/scala/uk/co/odinconsultants/fp/cats/state/sharedstate.scala
+++ src/main/scala/uk/co/odinconsultants/fp/cats/state/sharedstate.scala
@@ -12,36 +12,33 @@ import scala.concurrent.duration._
  */
 object sharedstate extends IOApp {
 
-  type MyState = List[String]
+  type MyState          = List[String]
   type MyStateContainer = Ref[IO, MyState]
 
   def putStrLn(str: String): IO[Unit] = IO(println(str))
 
-  def process1(myState: MyStateContainer): IO[Unit] = {
+  def process1(ref: MyStateContainer): IO[Unit] =
     putStrLn("Starting process #1") *>
-      IO.sleep(5.seconds) *>
-      myState.update(_ ++ List("#1")) *>
-      putStrLn("Done #1")
-  }
-
-  def process2(myState: MyStateContainer): IO[Unit] = {
-    putStrLn("Starting process #2") *>
       IO.sleep(3.seconds) *>
-      myState.update(_ ++ List("#2")) *>
-      putStrLn("Done #2")
-  }
+      ref.update(_ ++ List("#1")) *>
+      putStrLn("Done #1")
 
-  def process3(myState: MyStateContainer): IO[Unit] = {
+  def process2(ref: MyStateContainer): IO[Unit] =
+    putStrLn("Starting process #2") *>
+      IO.sleep(1.seconds) *>
+      ref.update(_ ++ List("#2")) *>
+      putStrLn("Done #2")
+
+  def process3(ref: MyStateContainer): IO[Unit] =
     putStrLn("Starting process #3") *>
-      IO.sleep(10.seconds) *>
-      myState.update(_ ++ List("#3")) *>
+      IO.sleep(2.seconds) *>
+      ref.update(_ ++ List("#3")) *>
       putStrLn("Done #3")
-  }
 
   def masterProcess: IO[Unit] =
-    Ref.of[IO, MyState](List.empty[String]).flatMap { myState =>
-      val ioa = List(process1(myState), process2(myState), process3(myState)).parSequence.void
-      ioa *> myState.get.flatMap(rs => putStrLn(rs.toString))
+    Ref.of[IO, MyState](List.empty[String]).flatMap { ref =>
+      val ioa = List(process1(ref), process2(ref), process3(ref)).parSequence.void
+      ioa *> ref.get.flatMap(rs => putStrLn(rs.toString))
     }
 
   override def run(args: List[String]): IO[ExitCode] =

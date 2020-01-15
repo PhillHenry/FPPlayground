c22507215ba52cc5fa81e5c059ea28db5360e218
PhillHenry <phillhenry@gmail.com>
2020-01-15 20:29:02 +0000

    Still broken but at least all the events happen. Unfortunately, assertion runs twice.

--- src/test/scala/uk/co/odinconsultants/fp/cats/fs2/queues/EventServiceSpec.scala
+++ src/test/scala/uk/co/odinconsultants/fp/cats/fs2/queues/EventServiceSpec.scala
@@ -3,7 +3,7 @@ package uk.co.odinconsultants.fp.cats.fs2.queues
 import cats.effect.laws.util.TestContext
 import cats.effect.{ContextShift, IO, Timer}
 import cats.implicits._
-import fs2.Stream
+import fs2.{Pipe, Stream}
 import fs2.concurrent.{SignallingRef, Topic}
 import org.scalatest.{Matchers, WordSpec}
 
@@ -22,25 +22,38 @@ class EventServiceSpec extends WordSpec with Matchers {
         Topic[IO, Event](Text("Initial Event")) product SignallingRef[IO, Boolean](false)
       }.flatMap { case (topic, signal) =>
         val service = new EventService[IO](topic, signal)
-        val s       = service.startPublisher.concurrently(service.startSubscribers)
-        (s.take(pivot) ++ assertOn(topic)) ++ s.drop(pivot)
+        val s: Stream[IO, Unit]       = service.startPublisher.concurrently(service.startSubscribers)
+        s.take(pivot) >> assertOn(topic) >> s
       }.compile.drain.unsafeToFuture()
 
-    def checkSubscribeSize(expected: Int)(topic: Topic[IO, Event]): Stream[IO, Unit] = topic.subscribers.flatMap { count =>
-      val check: IO[Unit] = if (count == expected) IO {
-        println(s"got expected count, $count")
-      } else {
-        println("raising error")
-        IO.raiseError(new Throwable(s"count = $count, expected $expected"))
+    def checkSubscribeSize(expected: Int)(topic: Topic[IO, Event]): Stream[IO, Unit] = {
+      val events: Stream[IO, Int] = topic.subscribers
+
+      val processEvent: Pipe[IO, Int, Unit] = { t =>
+        println("processEvent")
+         t.flatMap { count =>
+          println(s"count = $count")
+          val check: IO[Unit] = if (count == expected) IO {
+            println(s"got expected count, $count")
+          } else {
+            println("raising error")
+            IO.raiseError(new Throwable(s"count = $count, expected $expected"))
+          }
+          Stream.eval(check)
+        }
       }
-      Stream.eval(check)
+
+      println(s"Streaming through")
+      Stream(
+        events.through(processEvent)
+      ).parJoin(1)
     }
 
-    "have 1 subscriber near the beginning" in {
+    "have 1 subscriber near the beginning" ignore {
       val f = runTest(checkSubscribeSize(1), 1)
 
       testContext.tick(30 seconds)
-      Await.result(f, 5 seconds)
+      Await.result(f, 2 seconds)
       f.isCompleted shouldBe true
       assert(testContext.state.lastReportedFailure == None)
     }

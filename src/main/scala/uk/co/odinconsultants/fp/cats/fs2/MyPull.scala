f156d7bf108ba688d463871294427f5c8000b0a7
PhillHenry <phillhenry@gmail.com>
2020-01-16 20:04:53 +0000

    OK, so effects are run but return values are something else...

--- src/main/scala/uk/co/odinconsultants/fp/cats/fs2/MyPull.scala
+++ src/main/scala/uk/co/odinconsultants/fp/cats/fs2/MyPull.scala
@@ -1,31 +1,35 @@
 package uk.co.odinconsultants.fp.cats.fs2
 
 import cats.effect.{ExitCode, IO, IOApp}
-import fs2.Stream
-import cats.implicits._
+import fs2.{Pure, Stream}
 
 object MyPull extends IOApp {
 
-  def streamOfNums(n: Int): Stream[IO, Int] = {
+  type IntStream = Stream[IO, Int]
+
+  def pureStreamOfNums(n: Int): Stream[IO, Int] = {
     val s = Stream.range(1, n+1)
-    val pivot = n /2
+    takeAndDrop(n, s)
+  }
+
+  private def takeAndDrop(n: Int, s: IntStream): IntStream = {
+    val pivot = n / 2
     s.take(pivot) ++ s.drop(n - pivot)
   }
 
-  def effectfulStreamOfNums(n: Int): Stream[IO, Int] = {
-    val s = Stream.range(1, n+1).covary[IO]
-    val pivot = n /2
-    s.take(pivot) ++ s.drop(n - pivot)
+  def streamOfNums(n: Int): IntStream = {
+    val s     = Stream.range(1, n + 1)
+    takeAndDrop(n, s)
   }
 
-  def ioStreamOfNums(n: Int): Stream[IO, Unit] = {
-    val s           = Stream.range(1, n+1).evalMap { x => IO { println(s"x = $x") } }
-    val pivot       = n /2
-    val resumeFrom = n - pivot
-    println(s"n = $n, pivot = $pivot, resumeFrom = $resumeFrom")
-    val dropped     = s.drop(resumeFrom)
-    val taken       = s.take(pivot)
-    taken ++ dropped
+  def ioStreamOfNums(n: Int): IntStream = {
+    val s = Stream.range(1, n + 1).evalMap { x =>
+      IO {
+        println(s"x = $x")
+        x
+      }
+    }
+    takeAndDrop(n, s)
   }
 
   override def run(args: List[String]): IO[ExitCode] = {
--- src/test/scala/uk/co/odinconsultants/fp/cats/fs2/MyPullSpec.scala
+++ src/test/scala/uk/co/odinconsultants/fp/cats/fs2/MyPullSpec.scala
@@ -7,11 +7,21 @@ class MyPullSpec extends WordSpec with Matchers {
   import MyPull._
 
   "Splicing" should {
-    "not effect stream" in {
-      streamOfNums(6).compile.toList.unsafeRunSync() shouldBe List(1,2,3,4,5,6)
+    val n         = 6
+    val expected  = (1 to n).toList
+
+    def unsafeRunSyncToList(s: IntStream): List[Int] = s.compile.toList.unsafeRunSync()
+
+    "not effect pure streams" in {
+      unsafeRunSyncToList(pureStreamOfNums(n)) shouldBe expected
     }
+
     "not effect effectful stream" in {
-      effectfulStreamOfNums(6).compile.toList.unsafeRunSync() shouldBe List(1,2,3,4,5,6)
+      unsafeRunSyncToList(streamOfNums(n)) shouldBe expected
+    }
+
+    "not effect IO stream" in {
+      unsafeRunSyncToList(ioStreamOfNums(n)) shouldBe expected
     }
   }
 

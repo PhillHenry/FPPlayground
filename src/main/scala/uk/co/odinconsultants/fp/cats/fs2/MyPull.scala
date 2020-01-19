0e2f448abd41c08cd5a936bc941156f85639d569
PhillHenry <phillhenry@gmail.com>
2020-01-19 11:42:12 +0000

    Cleaned up. Note the values and the effects are all correct.

--- src/main/scala/uk/co/odinconsultants/fp/cats/fs2/MyPull.scala
+++ src/main/scala/uk/co/odinconsultants/fp/cats/fs2/MyPull.scala
@@ -43,37 +43,24 @@ object MyPull extends IOApp {
   import fs2._
   // see https://fs2.io/guide.html#statefully-transforming-streams
   def pullStreamOfNums(n: Int): IntStream = {
-    val s     = effectfulStream(n)
-    val pivot = n / 2
+    val s         = effectfulStream(n)
+    val pivot     = n / 2
+    val debugVal  = -1
 
     val debug: IO[Int] = IO.delay {
       println("spliced here")
-      -1
+      debugVal
     }
     val debugStream: IntStream = Stream.eval(debug)
 
-    type MyPullT[T]   = Pull[IO, T, Unit]
-    type MyPull       = MyPullT[Int]
     type ChunkStream  = (Chunk[Int], IntStream)
-    type Unconsed     = Pull[IO, INothing, Option[ChunkStream]]
-
-    val maybeChunkStream: Option[ChunkStream] => Pull[IO, Int, Unit] = _ match {
-      case None => Pull.done
-      case Some((c, s)) =>
-        println(s"c = $c, s = $s")
-        val remaining = s ++ debugStream // if (c.isEmpty) Stream.empty else Stream.emit(c.head.get)
-        remaining.pull.echo
-    }
-
 
     def injectIntoStream(s: Stream[IO, Int], n: Int): Stream[IO, Int] = {
 
       val toPull: Option[ChunkStream] => Pull[IO, Int, Unit] = _ match {
         case None =>
-          println("finished")
           Pull.pure(None)
         case Some((c, s)) =>
-          println(s"c = $c, s = $s")
           val acc = if (n == 1) debugStream ++ s else s
           Pull.suspend((injectIntoStream(acc, n - 1).consChunk(c)).pull.echo)
       }
@@ -83,22 +70,7 @@ object MyPull extends IOApp {
       }.void.stream
     }
 
-
-    //    val head:         IntStream = s.pull.take(pivot).void.stream
-//    val pullStream = s.pull.uncons.map(_ => None).flatMap(maybeChunkStream).stream
-//    pullStream ++
-//      Pull.output(s.pull.drop(pivot)).void.stream
-//      debugStream ++
-//      s.pull.drop(pivot).void.stream            // List(1,2,3)
-//      pullStream.pull.uncons.flatMap(toPull).stream // This is just the code in Stream.drop. Output = List(1,2,3,4,5,6) but also prints out 123123456
-//        s.drop(pivot) // List(1,2,3,4,5,6) but also prints out 123123456
-
-//    pullStream
-
-//    s.covary[Pure].take(pivot) ++ s.covary[Pure].drop(pivot)
-//    /*s.pull.take(pivot).void.stream ++*/ s.pull.takeRight(pivot).flatMap(c => Stream.emit(c.iterator).flatMap(xs => Stream.emit(xs.map(x => IO(x))))).stream
-//    injectAt(s.pull.uncons, pivot).void.stream
-    injectIntoStream(s, pivot).filter(_ != -1)
+    injectIntoStream(s, pivot).filter(_ != debugVal)
   }
 
   override def run(args: List[String]): IO[ExitCode] = {

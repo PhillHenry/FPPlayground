a9c070952ef1bcce9e1a59f0efd2b89cb529cfaa
PhillHenry <phillhenry@gmail.com>
2020-01-25 10:10:11 +0000

    Good use of TestContext.

--- src/main/scala/uk/co/odinconsultants/fp/cats/fs2/GroupFunctions.scala
+++ src/main/scala/uk/co/odinconsultants/fp/cats/fs2/GroupFunctions.scala
@@ -12,6 +12,15 @@ object GroupFunctions {
 
   def groupingAdjacentBy(s: Stream[IO, KV]): Stream[IO, (K, Chunk[KV])] = s.groupAdjacentBy(_._1)
 
+  type UniqueGroups[K, V] = Map[K, Set[V]]
+  def grouping[K, V](c: Chunk[(K, V)], by: K => K): UniqueGroups[K, V] = {
+    val seed: Map[K, Set[V]] = Map.empty[K, Set[V]].withDefaultValue(Set.empty[V])
+    c.foldLeft(seed) { case (a, (k, v)) =>
+      val newVal: Set[V] = a(k) + v
+        a + (k -> newVal)
+    }
+  }
+
 //  def groupingBy(s: Stream[IO, KV]): Stream[IO, (K, Chunk[KV])] = s.groupWithin(_._1)
 
 }
--- src/test/scala/uk/co/odinconsultants/fp/cats/fs2/GroupFunctionsSpec.scala
+++ src/test/scala/uk/co/odinconsultants/fp/cats/fs2/GroupFunctionsSpec.scala
@@ -1,11 +1,21 @@
 package uk.co.odinconsultants.fp.cats.fs2
 
-import cats.effect.IO
+import cats.effect.{ContextShift, IO, Timer}
+import cats.effect.laws.util.TestContext
 import org.scalatest.{Matchers, WordSpec}
 import fs2.{Chunk, Stream}
 import cats.implicits._
 
+import scala.concurrent.Await
+import scala.concurrent.duration._
+
 class GroupFunctionsSpec extends WordSpec with Matchers {
+
+
+  implicit val testContext: TestContext       = TestContext()
+  implicit val cs:          ContextShift[IO]  = testContext.contextShift(IO.ioEffect)
+  implicit val timer:       Timer[IO]         = testContext.timer(IO.ioEffect)
+
 
   import GroupFunctions._
 
@@ -16,14 +26,18 @@ class GroupFunctionsSpec extends WordSpec with Matchers {
   val kvs: Seq[KV] = Seq(1 -> "one", 2 -> "two", 1 -> "one again")
 
   s"Sequence ${kvs.mkString(", ")}" should {
-    val s: Stream[IO, KV] = Stream.fromIterator[IO](kvs.iterator)
+    val s: Stream[IO, KV] = Stream.fromIterator[IO](kvs.iterator).evalMap { kv => IO {
+      println(s"kv = $kv")
+      kv
+    }}
 
-    "be grouped by key" ignore {
-      val sGrouped: Stream[IO, (K, Chunk[KV])] = groupingAdjacentBy(s)
-      val grouped = sGrouped.compile.toList.unsafeRunSync()
-      withClue(grouped.mkString(", ")) {
-        grouped should have size kvs.map(_._1).toSet.size
-      }
+    "be grouped by key within a chunk" in {
+      val sGrouped: Stream[IO, UniqueGroups[K, V]] = s.groupWithin(kvs.size, 1.second).map(c => grouping(c, identity[K]))
+      val f = sGrouped.compile.toList.unsafeToFuture()
+      testContext.tick(1.second)
+      val chunks = Await.result(f, 1.second)
+      chunks should have size 1
+      chunks(0) should have size kvs.map(_._1).toSet.size
     }
   }
 

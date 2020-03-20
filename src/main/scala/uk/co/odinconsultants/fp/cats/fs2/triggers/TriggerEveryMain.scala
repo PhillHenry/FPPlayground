f8ca7a753385379857c12bf9227aa08b7ea7cc6a
Phillip Henry <PhillHenry@gmail.com>
2020-03-20 16:04:34 +0000

    Added some logging. Still not quite sure what's going on.

--- src/main/scala/uk/co/odinconsultants/fp/cats/fs2/triggers/TriggerEveryMain.scala
+++ src/main/scala/uk/co/odinconsultants/fp/cats/fs2/triggers/TriggerEveryMain.scala
@@ -8,11 +8,19 @@ import cats.implicits._
 
 object TriggerEveryMain extends IOApp {
   val delayStream = Stream
-    .iterate[IO, Int](
-      1)(_ + 1)
-    .metered(10.seconds)
+    .iterate[IO, Int](1)(_ + 1)
+    .metered(3.seconds)
 
   override def run(args: List[String]): IO[ExitCode] = {
-    delayStream.switchMap(_ => Stream.awakeEvery[IO](1 second)).compile.toList *> IO(ExitCode.Success)
+    val waker: Stream[IO, FiniteDuration] = Stream.awakeEvery[IO](1 second).map { x =>
+      println(s"Wake up! It's been ${x._1} now")
+      x
+    }
+    val triggered: Stream[IO, FiniteDuration] = delayStream.switchMap(_ => waker)
+    val loggedTrigger = triggered.flatMap { x =>
+      val s: Stream[IO, Unit] = Stream.eval(IO { println(s"x = $x") } )
+      s
+    }
+    loggedTrigger.take(3).compile.toList *> IO(ExitCode.Success)
   }
 }

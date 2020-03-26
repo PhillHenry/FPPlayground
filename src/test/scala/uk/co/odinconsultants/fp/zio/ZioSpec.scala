5a7464072ded5fe09d5b563ee7794949bf4ae457
Phillip Henry <PhillHenry@gmail.com>
2020-03-26 15:11:13 +0000

    Very clever indeed. ZIO Test knows when there is a bad assertion.

--- src/test/scala/uk/co/odinconsultants/fp/zio/ZioSpec.scala
+++ src/test/scala/uk/co/odinconsultants/fp/zio/ZioSpec.scala
@@ -9,19 +9,29 @@ import zio.test.TestAspect._
 
 object ZioSpec extends DefaultRunnableSpec {
 
-  val unhappyPath: Either[Throwable, String] = Left(new Exception("The unhappy path"))
+  val unhappyEitherPath: Either[Throwable, String] = Left(new Exception("The unhappy path"))
+
+  val unhappyOption: Option[String] = None
 
   override def spec: ZSpec[TestEnvironment, Any] = suite("Me playing around with ZIO test")(
     testM("1=1 obviously passes") {
       val shouldSatisfy:  Assertion[Int] => TestResult  = assert(1)
       val assertion:      Assertion[Any]                = equalTo(1)
       ZIO(shouldSatisfy(assertion))
-    },
+    }
+      ,
     testM("Problem I faced that is documented at http://javaagile.blogspot.com/2015/03/interesting-syntax-but-does-it-makes.html"){
-      val result:   Either[Throwable, String]     = unhappyPath
-      val asserted: Either[Throwable, TestResult] = result.map(x => assert(x)(equalTo("this is naive as it assumes happy path")))
-      val zio: ZIO[Any, Throwable, TestResult]    = ZIO.fromEither(asserted)
-      zio
+      val result:   Option[String]            = unhappyOption
+      val asserted: Option[TestResult]        = result.map(x => assert(x)(equalTo("this is naive as it assumes happy path")))
+      val myZio:    zio.IO[Unit, TestResult]  = ZIO.fromOption(asserted)
+      myZio
+    } @@ ignore // ignore because this test is deliberately imperfect
+      ,
+    testM("Problem I faced that is documented at http://javaagile.blogspot.com/2015/03/interesting-syntax-but-does-it-makes.html"){
+      val result:   Either[Throwable, String]       = unhappyEitherPath
+      val asserted: Either[Throwable, TestResult]   = result.map(x => assert(x)(equalTo("this is naive as it assumes happy path")))
+      val myZio:    ZIO[Any, Throwable, TestResult] = ZIO.fromEither(asserted)
+      myZio
     } @@ ignore // ignore because this test is deliberately imperfect
   )
 }

db71c4efddfb26e672f6f0be9af8923b5505becb
Phillip Henry <PhillHenry@gmail.com>
2020-03-26 14:24:44 +0000

    More explicit.

--- src/test/scala/uk/co/odinconsultants/fp/zio/ZioSpec.scala
+++ src/test/scala/uk/co/odinconsultants/fp/zio/ZioSpec.scala
@@ -2,13 +2,15 @@ package uk.co.odinconsultants.fp.zio
 
 import zio.ZIO
 import zio.test.Assertion.equalTo
-import zio.test.{DefaultRunnableSpec, ZSpec, assert, suite, testM}
+import zio.test.{Assertion, DefaultRunnableSpec, TestResult, ZSpec, assert, suite, testM}
 import zio.test.environment.TestEnvironment
 
 object ZioSpec extends DefaultRunnableSpec {
   override def spec: ZSpec[TestEnvironment, Any] = suite("Me playing around with ZIO test")(
     testM("1=1 obviously passes") {
-      ZIO(assert(1)(equalTo(1)))
+      val shouldSatisfy:  Assertion[Int] => TestResult = assert(1)
+      val assertion:      Assertion[Any] = equalTo(1)
+      ZIO(shouldSatisfy(assertion))
     }
   )
 }

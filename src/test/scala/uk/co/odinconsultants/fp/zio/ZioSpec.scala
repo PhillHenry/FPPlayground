9c465d3adb02f04712b00f4be62914c0217020ad
Phillip Henry <PhillHenry@gmail.com>
2020-03-26 15:04:25 +0000

    Addresses issues with not asserting the unhappy path of an effect.

--- src/test/scala/uk/co/odinconsultants/fp/zio/ZioSpec.scala
+++ src/test/scala/uk/co/odinconsultants/fp/zio/ZioSpec.scala
@@ -4,13 +4,24 @@ import zio.ZIO
 import zio.test.Assertion.equalTo
 import zio.test.{Assertion, DefaultRunnableSpec, TestResult, ZSpec, assert, suite, testM}
 import zio.test.environment.TestEnvironment
+import zio.test._
+import zio.test.TestAspect._
 
 object ZioSpec extends DefaultRunnableSpec {
+
+  val unhappyPath: Either[Throwable, String] = Left(new Exception("The unhappy path"))
+
   override def spec: ZSpec[TestEnvironment, Any] = suite("Me playing around with ZIO test")(
     testM("1=1 obviously passes") {
-      val shouldSatisfy:  Assertion[Int] => TestResult = assert(1)
-      val assertion:      Assertion[Any] = equalTo(1)
+      val shouldSatisfy:  Assertion[Int] => TestResult  = assert(1)
+      val assertion:      Assertion[Any]                = equalTo(1)
       ZIO(shouldSatisfy(assertion))
-    }
+    },
+    testM("Problem I faced that is documented at http://javaagile.blogspot.com/2015/03/interesting-syntax-but-does-it-makes.html"){
+      val result:   Either[Throwable, String]     = unhappyPath
+      val asserted: Either[Throwable, TestResult] = result.map(x => assert(x)(equalTo("this is naive as it assumes happy path")))
+      val zio: ZIO[Any, Throwable, TestResult]    = ZIO.fromEither(asserted)
+      zio
+    } @@ ignore // ignore because this test is deliberately imperfect
   )
 }

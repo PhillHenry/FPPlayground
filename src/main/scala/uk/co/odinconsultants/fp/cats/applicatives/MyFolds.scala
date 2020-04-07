e16d349810aedbb9e4d157aace957b92c5c44e22
Phillip Henry <PhillHenry@gmail.com>
2020-04-07 05:59:55 +0100

    fold, foldM and foldA

--- src/main/scala/uk/co/odinconsultants/fp/cats/applicatives/MyFolds.scala
+++ src/main/scala/uk/co/odinconsultants/fp/cats/applicatives/MyFolds.scala
@@ -6,11 +6,14 @@ import cats.implicits._
 object MyFolds {
 
   def main(args: Array[String]): Unit = {
-    List(5.some, 6.some, 7.some).foldMap(identity) // Some(18)
-//    List(5.some, 6.some, 7.some).foldMapA(identity) // Some(18)
+    foldFoldAFoldM(List(5.some, 6.some, 7.some))        // All are Some(18)
 
-    List(5.some, 6.some, None, 7.some).foldMap(identity) // Some(18)
-//    List(5.some, 6.some, None, 7.some).foldMapA(identity) // None
+    foldFoldAFoldM(List(5.some, 6.some, None, 7.some))  // foldA and foldM are Nones
   }
 
+  private def foldFoldAFoldM(options: List[Option[Int]]) = {
+    println(options.foldMap(identity))
+    println(options.foldMapA(identity))
+    println(options.foldMapM(identity))
+  }
 }

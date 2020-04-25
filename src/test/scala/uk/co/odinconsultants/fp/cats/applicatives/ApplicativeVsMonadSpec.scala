f3d10c506c27541719a7d493bf310f1e5ecd236f
Phillip Henry <PhillHenry@gmail.com>
2020-04-25 12:23:39 +0100

    More assertions.

--- src/test/scala/uk/co/odinconsultants/fp/cats/applicatives/ApplicativeVsMonadSpec.scala
+++ src/test/scala/uk/co/odinconsultants/fp/cats/applicatives/ApplicativeVsMonadSpec.scala
@@ -1,10 +1,8 @@
 package uk.co.odinconsultants.fp.cats.applicatives
 
-import cats.Applicative
+import cats.implicits._
 import org.scalatest.{Matchers, WordSpec}
 import uk.co.odinconsultants.fp.cats.validation.{EitherFixture, ValidatedFixture}
-import cats.implicits._
-import cats.effect.IO
 
 
 class ApplicativeVsMonadSpec extends WordSpec with Matchers {
@@ -13,7 +11,9 @@ class ApplicativeVsMonadSpec extends WordSpec with Matchers {
 
   "Applicatives" should {
     "be composed " in new ValidatedFixture {
-      valid1.myApplicativeExtensionMethod(valid2) shouldBe valid2
+      valid1.myApplicativeExtensionMethod(valid2)   shouldBe valid2
+      valid1.myApplicativeExtensionMethod(invalid1) shouldBe invalid1
+      invalid1.myApplicativeExtensionMethod(valid1) shouldBe invalid1
 //      valid1.myMonadExtensionMethod(valid2) does not compile because Validated is not a Monad
     }
   }
@@ -21,7 +21,9 @@ class ApplicativeVsMonadSpec extends WordSpec with Matchers {
   "Monads" should {
     "be composed" in new EitherFixture {
       valid1.myApplicativeExtensionMethod(valid2) shouldBe valid2
-      valid1.myMonadExtensionMethod(valid2) shouldBe valid2
+      valid1.myMonadExtensionMethod(valid2)       shouldBe valid2
+      valid1.myMonadExtensionMethod(invalid1)     shouldBe invalid1
+      invalid1.myMonadExtensionMethod(valid1)     shouldBe invalid1
     }
   }
 

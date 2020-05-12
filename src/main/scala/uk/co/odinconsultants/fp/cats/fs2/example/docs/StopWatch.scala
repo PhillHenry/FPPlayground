75814e6f9c640c2e2109734219177b0f5b575caa
Phillip Henry <PhillHenry@gmail.com>
2020-05-12 06:05:27 +0100

    Getting my head around streams - part deux.

--- src/main/scala/uk/co/odinconsultants/fp/cats/fs2/example/docs/StopWatch.scala
+++ src/main/scala/uk/co/odinconsultants/fp/cats/fs2/example/docs/StopWatch.scala
@@ -18,7 +18,13 @@ object StopWatch {
   def create[F[_] : Concurrent : Timer]: Stream[F, StopWatch[F]] =
     Stream.eval(Ref[F].of(0)).flatMap { c =>
       val api = new StopWatch[F] {
-        def elapsedSeconds: F[Int] = c.get
+        def elapsedSeconds: F[Int] = {
+          println(s"elapsedSeconds")
+          c.get.map { x =>
+            println(s"elapsedSeconds map = $x")
+            x
+          }
+        }
       }
       val process = Stream.fixedRate(1.second).evalMap(_ => c.update(_ + 1))
       Stream.emit(api).concurrently(process)
@@ -31,13 +37,24 @@ object StopWatchMain extends IOApp {
   import StopWatch._
 
   override def run(args: List[String]): IO[ExitCode] = {
-    aboutToSleepYieldThenNothing *>
-    printsTime1
+    /*
+    Fabio Labella @SystemFw May 11 15:03
+    Nothing will run until you compile.
+     */
+    val s = create[IO]
+    aboutToSleepYieldThenNothing(s) *>
+      aboutToSleepYieldThenNothing(s) *>
+      IO { println("\n\n")} *>
+      printsTime1(s) *>
+      printsTime1(s) *>
+      IO { println("\n\n")} *>
+      useResource(s) *>
+      useResource(s)
   }
 
-  def printsTime1: IO[ExitCode] = {
+  def printsTime1(s: Stream[IO, StopWatch[IO]]): IO[ExitCode] = {
     val io: IO[Int] = for {
-      stopWatch <- create[IO].zip(sleepingStream).map(_._1).compile.toList
+      stopWatch <- s.zip(sleepingStream).map(_._1).compile.toList
       xs        = stopWatch.map(_.elapsedSeconds)
       time      <- xs.last
     } yield {
@@ -48,12 +65,34 @@ object StopWatchMain extends IOApp {
     io *> IO(ExitCode.Success)
   }
 
+  def useResource(s: Stream[IO, StopWatch[IO]]): IO[ExitCode] = {
+    val io: Resource[IO, List[IO[Int]]] = for {
+      stopWatch <- s.zip(sleepingStream).map(_._1).compile.resource.toList
+      xs        = stopWatch.map(_.elapsedSeconds)
+    } yield {
+      xs.map{ io =>
+        val printIO = io.map { time =>
+          println(s"time = $time |xs| = ${xs.length}")
+          time
+        }
+        printIO
+      }
+    }
+
+    val y: IO[List[Int]] = io.use(_.sequence).map { x =>
+      println(s"x = $x")
+      x
+    }
+
+    y *> IO(ExitCode.Success)
+  }
+
   /**
    * From Stream#resource:
    * "Note that `create` returns a `Stream[F, StopWatch[F]]`, even
    * though there is only one instance being emitted: this is less than ideal,"
    */
-  def aboutToSleepYieldThenNothing: IO[ExitCode] = {
+  def aboutToSleepYieldThenNothing(s: Stream[IO, StopWatch[IO]]): IO[ExitCode] = {
     /**
      *  "so we might think about returning an `F[StopWatch[F]]` with the following code
      *
@@ -65,10 +104,10 @@ object StopWatchMain extends IOApp {
      * which causes `concurrently` to stop the `process` stream. As a  result, `elapsedSeconds`
      * never gets updated."
      */
-    sleepyStopWatch(create[IO]).take(5).compile.drain.map(_ => ExitCode.Success) // "yield" but no "flatMap". "About to sleep" but no "sleep over"
+    sleepyStopWatch(s).take(5).compile.drain.map(_ => ExitCode.Success) // "yield" but no "flatMap". "About to sleep" but no "sleep over"
   }
 
-  def sleepyStopWatch(s: Stream[IO, StopWatch[IO]]): Stream[IO, IO[Unit]] = for {
+  def sleepyStopWatch(s: Stream[IO, StopWatch[IO]]): Stream[IO, IO[Int]] = for {
     stopWatch <- s.zip(sleepingStream).map(_._1)
   } yield {
     println("yield")
@@ -76,6 +115,7 @@ object StopWatchMain extends IOApp {
       println("flatMap")
       IO {
         println(s"t = $t")
+        t
       }
     }
   }

8012c26819d5c8a2be54fcf1e956ab1eb3f7e552
Phillip Henry <PhillHenry@gmail.com>
2020-05-19 16:04:30 +0100

    Woo-hoo. Threading.

--- src/test/scala/uk/co/odinconsultants/fp/zio/streams/PipeMainSpec.scala
+++ src/test/scala/uk/co/odinconsultants/fp/zio/streams/PipeMainSpec.scala
@@ -2,19 +2,9 @@ package uk.co.odinconsultants.fp.zio.streams
 
 import java.io.{ByteArrayInputStream, ByteArrayOutputStream}
 
-import zio.test.{DefaultRunnableSpec, ZSpec, suite, testM}
-import zio.blocking.{Blocking, effectBlockingInterrupt}
-import zio.clock.Clock
-import zio.{App, Chunk, Schedule, UIO, ZIO}
-import zio.stream._
-import zio.console._
-import zio.duration._
-import zio.test.environment._
-import zio.duration._
-import zio.{RIO, Schedule, Queue, UIO, URIO}
-import zio.console._
-import zio.test._
 import zio.test.Assertion._
+import zio.test.environment._
+import zio.test.{DefaultRunnableSpec, ZSpec, suite, testM, _}
 
 object PipeMainSpec extends DefaultRunnableSpec {
 
@@ -29,7 +19,8 @@ object PipeMainSpec extends DefaultRunnableSpec {
         val inStream  = new ByteArrayInputStream(bytes)
         val outStream = new ByteArrayOutputStream(bufferSize)
         for {
-          _ <- doPipe(inStream, outStream)
+          p <- doPipe(inStream, outStream).fork
+          _ <- p.join
         } yield {
           assert(new String(outStream.toByteArray))(equalTo(msg))
         }

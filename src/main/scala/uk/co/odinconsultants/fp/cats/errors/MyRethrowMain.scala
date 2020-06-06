d8020e4ce302fea4cc3202b9f655a9310945c8fc
Phillip Henry <PhillHenry@gmail.com>
2020-06-06 08:33:24 +0100

    Bits taken from Gitter

---
+++ src/main/scala/uk/co/odinconsultants/fp/cats/errors/MyRethrowMain.scala
@@ -0,0 +1,38 @@
+package uk.co.odinconsultants.fp.cats.errors
+
+import cats.effect.{IO, IOApp}
+import cats.effect.{ExitCode, IO, IOApp, Timer}
+import scala.concurrent.duration._
+import cats.implicits._
+
+object MyRethrowMain extends IOApp {
+
+  case class Table(x: String)
+
+  val trigger = "blow up!"
+
+  def loadTable(table: Table): IO[Table] = IO {
+    if (table.x == trigger)
+      throw new Exception(trigger)
+    else
+      Table(s"loaded ${table.x}")
+  }
+
+  override def run(args: List[String]): IO[ExitCode] = {
+    val tableList = List(Table("1"), Table("2"), Table(trigger), Table("3)"))
+
+    val tapFunction: Either[Throwable, Table] => IO[Unit] = _ match {
+      case Left(x)  => IO(println(s"Blew up on tapping with $x"))
+      case Right(x) => IO(println(s"tapped $x"))
+    }
+
+    val io = tableList.parTraverse_ { t =>
+      loadTable(t).attempt.flatTap(tapFunction).rethrow
+    }
+    // Gavin Bisesi @Daenyth Jun 05 18:06 (typelevel/cats-effect Gitter)
+    //  ^ simple and straightforward
+    //    the attempt/rethrow thing is almost .guaranteeCase except that guarantee lets you handle "cancelled" too
+    //  whereas attempt/rethrow means that cancel also cancels the flatTap part
+    io.as(ExitCode.Success)
+  }
+}
--- src/main/scala/uk/co/odinconsultants/fp/cats/kleisli/MyKleisli.scala
+++ src/main/scala/uk/co/odinconsultants/fp/cats/kleisli/MyKleisli.scala
@@ -1,9 +1,9 @@
 package uk.co.odinconsultants.fp.cats.kleisli
 
-import cats.data.Kleisli
+import cats.Monad
+import cats.data.{Kleisli, OptionT}
 
 import scala.util.Try
-
 import cats.implicits._
 
 object MyKleisli {
@@ -17,6 +17,47 @@ object MyKleisli {
     val combined:     Kleisli[Option, String, Double] = kStringToInt.andThen(kDivision)
 
     println(combined.run("5")) // Some(2.0)
+  }
+
+  /**
+  Ben Spencer @dangerousben 14:16
+I probably just don't know Kleisli well enough, but it feels like some stuff is easier with plain functions and some with kleislis
+trying to implement something shaped like this, and it feels like it should be a lot easier than it's proving to be:
+(A => Option[B], B => F[C], C => F[D]) => Kleisli[OptionT[F, *], A, D]
+my inclination is to not bother applying Kleisli or OptionT until the end, but that means giving up on some useful facilities
+
+Fabio Labella @SystemFw 14:21
+follow your taste
+going to the full kleisli first is probably helpful in deriving what the code should be, but it might not be the prettiest/more economical way
+
+Fabio Labella @SystemFw 14:38
+@dangerousben
+
+one way
+   */
+  def foo[F[_]: Monad, A, B, C, D](
+                                    a: A => Option[B],
+                                    b: B => F[C],
+                                    c: C => F[D]
+                                  ): Kleisli[OptionT[F, *], A, D] = {
+
+    def lift[X, Y] =
+      Kleisli(_: X => F[Y]).mapK(OptionT.liftK)
+
+    Kleisli { in: A =>
+      OptionT(a(in).pure[F])
+    } >>> lift(b) >>> lift(c)
+  }
+
+  def fooEquivalently[F[_]: Monad, A, B, C, D](
+                                    a: A => Option[B],
+                                    b: B => F[C],
+                                    c: C => F[D]
+                                  ): Kleisli[OptionT[F, *], A, D] = Kleisli { in =>
+    OptionT
+      .fromOption[F](a(in))
+      .semiflatMap(b)
+      .semiflatMap(c)
   }
 
 }

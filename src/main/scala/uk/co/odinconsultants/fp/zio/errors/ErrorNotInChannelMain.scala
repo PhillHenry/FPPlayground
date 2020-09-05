ec9d2de2c84887619bb262af50dba11e9d7a1268
Phillip Henry <PhillHenry@gmail.com>
2020-09-05 10:57:47 +0100

    Using effectTotal when it's not (ie, lying) causes a fibre failure.

--- src/main/scala/uk/co/odinconsultants/fp/zio/errors/ErrorNotInChannelMain.scala
+++ src/main/scala/uk/co/odinconsultants/fp/zio/errors/ErrorNotInChannelMain.scala
@@ -1,5 +1,7 @@
 package uk.co.odinconsultants.fp.zio.errors
 
+import java.io
+
 import zio._
 
 object ErrorNotInChannelMain extends App {
@@ -8,7 +10,8 @@ object ErrorNotInChannelMain extends App {
 
   override def run(args: List[String]): ZIO[ZEnv, Nothing, Int] = {
 //    (withExpectedErrorType *> throwingException).catchAll(logError).as(0) // "test"
-    (throwingException *> withExpectedErrorType).catchAll(logError).as(0)   // "java.lang.Exception: where does this go to?"
+    val combined: ZIO[Any, java.io.Serializable, Nothing] = throwingException *> withExpectedErrorType
+    combined.catchAll(logError).as(0)   // "java.lang.Exception: where does this go to?"
   }
 
   def logError(x: Any): UIO[Unit] = UIO(println(x))
@@ -16,6 +19,14 @@ object ErrorNotInChannelMain extends App {
   def withExpectedErrorType: IO[MyError, Nothing] = ZIO.fail(MyError("test"))
 
   def throwingException: Task[Nothing] = ZIO {
+    throw new Exception("where does this go to?")
+  }
+
+  /**
+  "Fiber failed.
+  An unchecked error was produced."
+   */
+  def evil: Task[Nothing] = UIO {
     throw new Exception("where does this go to?")
   }
 

d4f4f88337be897ed348bcc0c51ad2dbdb558063
Phillip Henry <PhillHenry@gmail.com>
2020-11-16 15:47:09 +0000

    Doh! Was being stupid.

--- src/main/scala/uk/co/odinconsultants/fp/lang/JoyOfSets.scala
+++ src/main/scala/uk/co/odinconsultants/fp/lang/JoyOfSets.scala
@@ -2,17 +2,16 @@ package uk.co.odinconsultants.fp.lang
 
 object JoyOfSets {
 
-  type Permutations[T] = List[List[T]]
+  type Permutations[T] = Seq[Seq[T]]
 
   def viaSet[T](xs: Permutations[T]): Permutations[T] = xs.map { xs =>
-    val aSet = xs.toSet
-    aSet.toList
+    xs.toSet.toList
   }
 
   def main(args: Array[String]): Unit = {
     val vals          = (1 to 6)
     val permutations  = vals.permutations.toList
-    val toSets        = permutations
+    val toSets        = viaSet(permutations)
     println(s"Number of unique permutations        = ${permutations.toSet.size}")
     println(s"Number of unique results from toSet  = ${toSets.toSet.size}")
     println(s"Sample toSet:\n${toSets.toSet.take(5).mkString("\n")}")

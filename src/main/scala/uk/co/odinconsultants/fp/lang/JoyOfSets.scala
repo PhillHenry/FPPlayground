e24402bd32b03b6a32f48e7a5081133e4f8c67a1
Phillip Henry <PhillHenry@gmail.com>
2020-11-16 15:45:07 +0000

    Now it manifests the problem!

--- src/main/scala/uk/co/odinconsultants/fp/lang/JoyOfSets.scala
+++ src/main/scala/uk/co/odinconsultants/fp/lang/JoyOfSets.scala
@@ -2,16 +2,20 @@ package uk.co.odinconsultants.fp.lang
 
 object JoyOfSets {
 
+  type Permutations[T] = List[List[T]]
+
+  def viaSet[T](xs: Permutations[T]): Permutations[T] = xs.map { xs =>
+    val aSet = xs.toSet
+    aSet.toList
+  }
+
   def main(args: Array[String]): Unit = {
-    val vals          = (1 to 10)
+    val vals          = (1 to 6)
     val permutations  = vals.permutations.toList
-    val toSets        = permutations.map { xs =>
-      val aSet = scala.collection.mutable.Set[Int]()
-      xs.foreach { x => aSet.add(x) }
-      aSet.toList
-    }
+    val toSets        = permutations
     println(s"Number of unique permutations        = ${permutations.toSet.size}")
     println(s"Number of unique results from toSet  = ${toSets.toSet.size}")
+    println(s"Sample toSet:\n${toSets.toSet.take(5).mkString("\n")}")
   }
 
 }

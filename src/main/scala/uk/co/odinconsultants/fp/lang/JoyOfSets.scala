27f3b4983f371b498f7905ab4b3657eddda0f97e
Phillip Henry <PhillHenry@gmail.com>
2020-11-16 16:10:29 +0000

    Also demonstrates the effect.

--- src/main/scala/uk/co/odinconsultants/fp/lang/JoyOfSets.scala
+++ src/main/scala/uk/co/odinconsultants/fp/lang/JoyOfSets.scala
@@ -1,9 +1,5 @@
 package uk.co.odinconsultants.fp.lang
 
-import java.util
-
-import scala.collection.mutable
-
 object JoyOfSets {
 
   type Permutations[T] = Seq[Seq[T]]
@@ -25,6 +21,14 @@ object JoyOfSets {
     println(s"Number of unique permutations        = ${permutations.toSet.size}")
     println(s"Number of unique results from toSet  = ${toSets.toSet.size}")
     println(s"Sample toSet:\n${toSets.toSet.take(5).mkString("\n")}")
+
+    val set1 = List(1, 5, 2, 4, 3, 6, 7).map(new MyIntWrapper(_)).toSet
+    val set2 = List(1, 5, 2, 4, 6, 3, 7).map(new MyIntWrapper(_)).toSet
+    val x = set1.toList
+    val y = set2.toList
+    println(s"x = ${x.mkString("")}")
+    println(s"y = ${y.mkString("")}")
+    println(x == y)
   }
 
 }

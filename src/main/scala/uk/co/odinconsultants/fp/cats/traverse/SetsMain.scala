62e23390bd1cb1762b39502a6bc2342ed20054a8
Phillip Henry <PhillHenry@gmail.com>
2020-11-17 16:19:53 +0000

    Full Set of Lists to a List of Sets using a CommutativeApplicative.

--- src/main/scala/uk/co/odinconsultants/fp/cats/traverse/SetsMain.scala
+++ src/main/scala/uk/co/odinconsultants/fp/cats/traverse/SetsMain.scala
@@ -2,25 +2,9 @@ package uk.co.odinconsultants.fp.cats.traverse
 
 import cats.CommutativeApplicative
 import cats.implicits._
-import uk.co.odinconsultants.fp.lang.JoyOfSets
-import cats.instances.set.catsStdInstancesForSet.unorderedTraverse
-import uk.co.odinconsultants.fp.lang.JoyOfSets.MyIntWrapper
 
 object SetsMain {
 
-  def traverseFn[T]: T => List[T]  = { t =>
-    println(s"t = $t")
-    List(t)
-  }
-
-  val commutativeApplicativeSet = new CommutativeApplicative[Set] {
-    override def pure[A](x: A): Set[A] = Set(x)
-
-    override def ap[A, B](ff: Set[A => B])(fa: Set[A]): Set[B] = for {
-      f <- ff
-      a <- fa
-    } yield f(a)
-  }
   val commutativeApplicativeList = new CommutativeApplicative[List] {
     override def pure[A](x: A): List[A] = List(x)
 
@@ -31,8 +15,9 @@ object SetsMain {
   }
 
   def main(args: Array[String]): Unit = {
-
-    val listOfSets: List[Set[MyIntWrapper]] = JoyOfSets.set1.unorderedTraverse(traverseFn)(commutativeApplicativeList)
+    val setOfLists: Set[List[Int]] = Set(1, 2, 3, 4, 5).map(List(_))
+    val listOfSets: List[Set[Int]] = setOfLists.unorderedTraverse(identity[List[Int]])(commutativeApplicativeList)
+    println(setOfLists)
     println(listOfSets)
   }
 

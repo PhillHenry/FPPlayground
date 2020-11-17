07fa8ec35dd5467eb94020f57312cf15b32fa404
Phillip Henry <PhillHenry@gmail.com>
2020-11-17 16:24:49 +0000

    Print statement and comment.

--- src/main/scala/uk/co/odinconsultants/fp/cats/traverse/SetsMain.scala
+++ src/main/scala/uk/co/odinconsultants/fp/cats/traverse/SetsMain.scala
@@ -3,15 +3,24 @@ package uk.co.odinconsultants.fp.cats.traverse
 import cats.CommutativeApplicative
 import cats.implicits._
 
+/**
+ * @see https://stackoverflow.com/questions/53945996/cats-effecthow-to-transform-mapx-ioy-to-iomapx-y
+ */
 object SetsMain {
 
   val commutativeApplicativeList = new CommutativeApplicative[List] {
-    override def pure[A](x: A): List[A] = List(x)
+    override def pure[A](x: A): List[A] = {
+      println(s"pure $x")
+      List(x)
+    }
 
     override def ap[A, B](ff: List[A => B])(fa: List[A]): List[B] = for {
       f <- ff
       a <- fa
-    } yield f(a)
+    } yield {
+      println(s"ap $a")
+      f(a)
+    }
   }
 
   def main(args: Array[String]): Unit = {

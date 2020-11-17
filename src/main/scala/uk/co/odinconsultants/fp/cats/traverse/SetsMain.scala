42bf877001ab503b5281ca583e94e77c21fcd5d7
Phillip Henry <PhillHenry@gmail.com>
2020-11-17 16:16:19 +0000

    Implemented CommutativeApplicative

--- src/main/scala/uk/co/odinconsultants/fp/cats/traverse/SetsMain.scala
+++ src/main/scala/uk/co/odinconsultants/fp/cats/traverse/SetsMain.scala
@@ -1,5 +1,6 @@
 package uk.co.odinconsultants.fp.cats.traverse
 
+import cats.CommutativeApplicative
 import cats.implicits._
 import uk.co.odinconsultants.fp.lang.JoyOfSets
 import cats.instances.set.catsStdInstancesForSet.unorderedTraverse
@@ -7,14 +8,32 @@ import uk.co.odinconsultants.fp.lang.JoyOfSets.MyIntWrapper
 
 object SetsMain {
 
-  def traverseFn[T]: T => Set[T]  = { t =>
+  def traverseFn[T]: T => List[T]  = { t =>
     println(s"t = $t")
-    Set(t)
+    List(t)
+  }
+
+  val commutativeApplicativeSet = new CommutativeApplicative[Set] {
+    override def pure[A](x: A): Set[A] = Set(x)
+
+    override def ap[A, B](ff: Set[A => B])(fa: Set[A]): Set[B] = for {
+      f <- ff
+      a <- fa
+    } yield f(a)
+  }
+  val commutativeApplicativeList = new CommutativeApplicative[List] {
+    override def pure[A](x: A): List[A] = List(x)
+
+    override def ap[A, B](ff: List[A => B])(fa: List[A]): List[B] = for {
+      f <- ff
+      a <- fa
+    } yield f(a)
   }
 
   def main(args: Array[String]): Unit = {
-//    implicit val ev = unorderedTraverse[MyIntWrapper]
-//    JoyOfSets.set1.unorderedTraverse(traverseFn)
+
+    val listOfSets: List[Set[MyIntWrapper]] = JoyOfSets.set1.unorderedTraverse(traverseFn)(commutativeApplicativeList)
+    println(listOfSets)
   }
 
 }

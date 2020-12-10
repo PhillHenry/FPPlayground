dd5d3024ede7a819af609bb4f64fc448ead4c208
Phillip Henry <PhillHenry@gmail.com>
2020-12-10 16:11:24 +0000

    More use cases, clearer intent.

--- src/test/scala/uk/co/odinconsultants/fp/cats/errors/ExceptionsInCatchesSpec.scala
+++ src/test/scala/uk/co/odinconsultants/fp/cats/errors/ExceptionsInCatchesSpec.scala
@@ -5,43 +5,55 @@ import org.scalatest.{Matchers, WordSpec}
 
 class ExceptionsInCatchesSpec extends WordSpec with Matchers {
 
-  val input                           = 1
-  val happyPathIO:  IO[Int]           = IO.pure(input)
+  val input                                     = 1
+  val happyPathIO:            IO[Int]           = IO.pure(input)
+  val releaseBarfs:           Resource[IO, Int] = resourceBlowsUpOnRelease(happyPathIO)
+  val makeAndReleaseBarfs:    Resource[IO, Int] = resourceBlowsUpOnRelease(IO { throw new Exception(makeErrorMessage)})
+  def unhappyPathIO(x: Int):  IO[Int]           = IO { throw new Exception(useErrorMessage(x))}
 
-  def unhappyPathIO(x: Int): IO[Int]  = IO { throw new Exception(useErrorMessage(x))}
-
-  val releaseBarfs: Resource[IO, Int] = resourceBlowsUpOnRelease(happyPathIO)
-  val makeBarfs:    Resource[IO, Int] = resourceBlowsUpOnRelease(unhappyPathIO(input))
-
-  "Try succeeds but finally barfs" should {
-    "be produce message in close" in {
-      val caught = intercept[Exception] {
-        releaseBarfs.use(x => IO { println(s"Success with $x")}).unsafeRunSync()
-      }
+  "'make' and 'use' succeeds but 'release' barfs" should {
+    "give the 'use' error and suppress the 'release' error" in {
+      val caught: Exception = useSucceedsButExpectErrorWhen(releaseBarfs)
       caught.getMessage shouldEqual releaseErrorMessage(input)
-      caught.getSuppressed should be (empty)
     }
   }
 
-  "Try fails and so does finally" should {
-
-    def expectUseErrorMessageForUnsafe(resource: Resource[IO, Int]): Exception = {
-      val caught = intercept[Exception] {
-        resource.use(unhappyPathIO).unsafeRunSync()
-      }
-      caught.getMessage shouldEqual useErrorMessage(input)
-      caught.getSuppressed should be(empty)
-      caught
-    }
-
-    "give the 'use'' error abd suppress the 'close' error" in {
-      expectUseErrorMessageForUnsafe(releaseBarfs)
-    }
-    "result in the same even with Resource.make being the unhappy path" in {
-      expectUseErrorMessageForUnsafe(makeBarfs)
+  "'use' succeeds but 'release' and 'make' barf" should {
+    "give the 'use' error and suppress the 'release' error" in {
+      val caught: Exception = useSucceedsButExpectErrorWhen(makeAndReleaseBarfs)
+      caught.getMessage shouldEqual makeErrorMessage
     }
   }
 
+  "'use' and 'release' failing" should {
+    "give the 'use' error and suppress the 'release' error" in {
+      expectUseErrorMessageForUnsafe(releaseBarfs).getMessage shouldEqual useErrorMessage(input)
+    }
+  }
+
+  "'use' and 'make' and 'release' failing" should {
+    "give the 'use' error and suppress the 'release' error" in {
+      expectUseErrorMessageForUnsafe(makeAndReleaseBarfs).getMessage shouldEqual makeErrorMessage
+    }
+  }
+
+  private def useSucceedsButExpectErrorWhen(resource: Resource[IO, Int]) = {
+    val caught = intercept[Exception] {
+      resource.use(x => IO {
+        println(s"Success with $x")
+      }).unsafeRunSync()
+    }
+    caught.getSuppressed should be(empty)
+    caught
+  }
+
+  def expectUseErrorMessageForUnsafe(resource: Resource[IO, Int]): Exception = {
+    val caught = intercept[Exception] {
+      resource.use(unhappyPathIO).unsafeRunSync()
+    }
+    caught.getSuppressed should be(empty)
+    caught
+  }
 
   private def resourceBlowsUpOnRelease(inputIO: IO[Int]): Resource[IO, Int] =
     Resource.make(inputIO)(x => IO {
@@ -50,5 +62,7 @@ class ExceptionsInCatchesSpec extends WordSpec with Matchers {
 
   private def useErrorMessage(x: Int) = s"Use failure with $x"
 
-  private def releaseErrorMessage(x: Int) = s"releasing $x"
+  private def releaseErrorMessage(x: Int) = s"release failing for $x"
+
+  private val makeErrorMessage = s"make failing"
 }

ef5f4582b445e1fa2b57431312078cafb07c56e4
Phillip Henry <PhillHenry@gmail.com>
2020-12-10 14:46:56 +0000

    Seems like an exception in use trumps everything.

--- src/test/scala/uk/co/odinconsultants/fp/cats/errors/ExceptionsInCatchesSpec.scala
+++ src/test/scala/uk/co/odinconsultants/fp/cats/errors/ExceptionsInCatchesSpec.scala
@@ -8,9 +8,12 @@ class ExceptionsInCatchesSpec extends WordSpec with Matchers {
   val input                           = 1
   val happyPathIO:  IO[Int]           = IO.pure(input)
 
-  "Try succeeds but finally barfs" should {
-    val releaseBarfs: Resource[IO, Int] = resourceBlowsUpOnRelease(happyPathIO)
+  def unhappyPathIO(x: Int): IO[Int]  = IO { throw new Exception(useErrorMessage(x))}
 
+  val releaseBarfs: Resource[IO, Int] = resourceBlowsUpOnRelease(happyPathIO)
+  val makeBarfs:    Resource[IO, Int] = resourceBlowsUpOnRelease(unhappyPathIO(input))
+
+  "Try succeeds but finally barfs" should {
     "be produce message in close" in {
       val caught = intercept[Exception] {
         releaseBarfs.use(x => IO { println(s"Success with $x")}).unsafeRunSync()
@@ -20,17 +23,25 @@ class ExceptionsInCatchesSpec extends WordSpec with Matchers {
     }
   }
 
-
   "Try fails and so does finally" should {
-    val releaseBarfs: Resource[IO, Int] = resourceBlowsUpOnRelease(happyPathIO)
-    "give the 'use'' error abd suppress the 'close' error" in {
+
+    def expectUseErrorMessageForUnsafe(resource: Resource[IO, Int]): Exception = {
       val caught = intercept[Exception] {
-        releaseBarfs.use(x => IO { throw new Exception(useErrorMessage(x))}).unsafeRunSync()
+        resource.use(unhappyPathIO).unsafeRunSync()
       }
       caught.getMessage shouldEqual useErrorMessage(input)
-      caught.getSuppressed should be (empty)
+      caught.getSuppressed should be(empty)
+      caught
+    }
+
+    "give the 'use'' error abd suppress the 'close' error" in {
+      expectUseErrorMessageForUnsafe(releaseBarfs)
+    }
+    "result in the same even with Resource.make being the unhappy path" in {
+      expectUseErrorMessageForUnsafe(makeBarfs)
     }
   }
+
 
   private def resourceBlowsUpOnRelease(inputIO: IO[Int]): Resource[IO, Int] =
     Resource.make(inputIO)(x => IO {

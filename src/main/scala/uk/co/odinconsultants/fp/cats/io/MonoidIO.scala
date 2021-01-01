4c0672b8a50e38e8363fd07e48a3167fdd415545
Phillip Henry <PhillHenry@gmail.com>
2021-01-01 12:53:32 +0000

    Monoids of IO seem to run the first effect and only the second if there was an exception in the first.

--- src/main/scala/uk/co/odinconsultants/fp/cats/io/MonoidIO.scala
+++ src/main/scala/uk/co/odinconsultants/fp/cats/io/MonoidIO.scala
@@ -17,7 +17,12 @@ object MonoidIO extends IOApp {
     List(2)
   }
 
+  val throwsException: IO[IOType] = IO.raiseError(new Throwable("boom!"))
+
   def combineAndPrint(fa: IO[IOType], fb: IO[IOType]): IO[Unit] = (fa <+> fb).flatMap(x => IO { println(s"x = $x") } )
 
-  override def run(args: List[String]): IO[ExitCode] = combineAndPrint(_1, _2).as(ExitCode.Success) // x = List(1) - doesn't even run _2
+  override def run(args: List[String]): IO[ExitCode] = {
+    combineAndPrint(_1, _2) *>                                  // x = List(1) - doesn't even run _2
+      combineAndPrint(throwsException, _2).as(ExitCode.Success) // x = List(2) and runs _2
+  }
 }

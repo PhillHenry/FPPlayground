836da64becfaef3943e1678fee9199242f5e966f
Phillip Henry <PhillHenry@gmail.com>
2021-01-01 12:51:02 +0000

    Monoids of IO.

--- src/main/scala/uk/co/odinconsultants/fp/cats/errors/MyRethrowMain.scala
+++ src/main/scala/uk/co/odinconsultants/fp/cats/errors/MyRethrowMain.scala
@@ -19,26 +19,27 @@ object MyRethrowMain extends IOApp {
   }
 
   override def run(args: List[String]): IO[ExitCode] = {
-    val tableList = List(Table("1"), Table(trigger), Table("2"), Table(trigger), Table("3)"))
+    val tableList = List(Table("1"), Table(trigger), Table("2"), Table(trigger), Table("3"))
 
     val tableFunction: Table => IO[Unit] = x =>  IO(println(s"tapped $x"))
 
     val attemptFunction: Either[Throwable, Table] => IO[Unit] = _ match {
       case Left(x)  => IO(println(s"Blew up on tapping with $x"))
-      case Right(x) =>tableFunction(x)
+      case Right(x) => tableFunction(x)
     }
 
     val io: IO[Unit] = tableList.parTraverse_ { t =>
-      val attempted: IO[Either[Throwable, Table]] = loadTable(t).attempt
+      val attempted:        IO[Either[Throwable, Table]] = loadTable(t).attempt
       val flatMapAttempted: IO[Either[Throwable, Table]] = attempted.flatTap(attemptFunction)
-      flatMapAttempted.rethrow // rethrow actually blows up the App!
-      val plainFlatMap: IO[Table] = loadTable(t).flatTap(tableFunction)
-      plainFlatMap // blows up the App!
+//      flatMapAttempted.rethrow // rethrow actually blows up the App!
+//      val plainFlatMap: IO[Table] = loadTable(t).flatTap(tableFunction)
+//      plainFlatMap // blows up the App!
+      flatMapAttempted
     }
     // Gavin Bisesi @Daenyth Jun 05 18:06 (typelevel/cats-effect Gitter)
     //  ^ simple and straightforward
     //    the attempt/rethrow thing is almost .guaranteeCase except that guarantee lets you handle "cancelled" too
     //  whereas attempt/rethrow means that cancel also cancels the flatTap part
-    io.as(ExitCode.Success)
+    (io *> IO { println("finished") }).as(ExitCode.Success)
   }
 }
---
+++ src/main/scala/uk/co/odinconsultants/fp/cats/io/MonoidIO.scala
@@ -0,0 +1,23 @@
+package uk.co.odinconsultants.fp.cats.io
+
+import cats.effect.{ExitCode, IO, IOApp}
+import cats.implicits._
+
+object MonoidIO extends IOApp {
+
+  type IOType = List[Int]
+
+  val _1: IO[IOType] = IO {
+    println(1)
+    List(1)
+  }
+
+  val _2: IO[IOType] = IO {
+    println(2)
+    List(2)
+  }
+
+  def combineAndPrint(fa: IO[IOType], fb: IO[IOType]): IO[Unit] = (fa <+> fb).flatMap(x => IO { println(s"x = $x") } )
+
+  override def run(args: List[String]): IO[ExitCode] = combineAndPrint(_1, _2).as(ExitCode.Success) // x = List(1) - doesn't even run _2
+}

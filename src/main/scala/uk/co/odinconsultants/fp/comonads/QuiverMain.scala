addbfa4fc662bdb18b00a6d76f49ddd6b0cc0aab
Phillip Henry <PhillHenry@gmail.com>
2021-01-19 13:46:15 +0000

    Traverses graph making the context the subgraph.

--- src/main/scala/uk/co/odinconsultants/fp/comonads/QuiverMain.scala
+++ src/main/scala/uk/co/odinconsultants/fp/comonads/QuiverMain.scala
@@ -11,27 +11,15 @@ import quiver._
  */
 object QuiverMain {
 
-//  class QuiverMain[V, E] {
-//
-//    def cobind[A, B](g: GDecomp[V, A, E])(
-//      f: GDecomp[V, A, E] => B): GDecomp[V, A, E] =
-//      GDecomp(g.ctx.copy(label = f(g)),
-//        g.rest.decompAny.toGDecomp.map { x =>
-//          val GDecomp(c, r) = cobind(x)(f)
-//          c & r
-//        } getOrElse empty)
-//  }
+  def makeNodes(n: Int): Seq[LNode[V, AA]] = (0 until n).map(i => LNode(s"v$i", AA(s"node label $i")))
 
-
-  def makeNodes(n: Int): Seq[LNode[V, A]] = (0 until n).map(i => LNode(s"v$i", A(s"node label $i")))
-
-  def chain(ns: Seq[LNode[V, A]]): Seq[LEdge[V, B]] = ns.tail.map { n => LEdge(n.vertex, ns.head.vertex, B("relationship")) }
+  def chain(ns: Seq[LNode[V, AA]]): Seq[LEdge[V, BB]] = ns.tail.map { n => LEdge(n.vertex, ns.head.vertex, BB("relationship")) }
 
   type V = String
   type E = Int
 
-  case class A(x: String)
-  case class B(x: String)
+  case class AA(x: String)
+  case class BB(x: String)
 
   def gdecompComonad[V,E] = new Comonad[λ[α => GDecomp[V,α,E]]] {
 
@@ -48,16 +36,21 @@ object QuiverMain {
 
 
   def main(args: Array[String]): Unit = {
+    val nodes: Seq[LNode[V, AA]] = makeNodes(5)
+    val edges: Seq[LEdge[V, BB]] = chain(nodes)
+    val g:     Graph[V, AA, BB]  = mkGraph(nodes, edges)
 
-    val nodes: Seq[LNode[V, A]] = makeNodes(5)
-    val edges: Seq[LEdge[V, B]] = chain(nodes)
-
-    val g:          Graph[V, A, B]  = mkGraph(nodes, edges)
-    val decomposed: Decomp[V, A, B] = g.decompAny
-
+    val subGraphs: Option[GDecomp[V, Context[V, AA, BB], BB]] = g.decompAny.toGDecomp.map { gDecomp =>
+      gdecompComonad.coflatMap(gDecomp)(_.ctx)
+    }
+    println(s"subGraphs of ${gDecompAsString(subGraphs.get)}")
 
     println(s"maxDegree = ${maxDegreePlain(g)}")
 
+  }
+
+  def gDecompAsString[V, A, B](x: GDecomp[V, A, B]): String = {
+    s"GDecomp\n${contextAsString(x.ctx)}\nRest:\n${x.rest}"
   }
 
   def contextGraph[V,N,E](g: Graph[V,N,E]): Graph[V,Context[V,N,E],E] = ???
@@ -67,19 +60,22 @@ object QuiverMain {
       z max (c.label.ins.size + c.label.outs.size)
     }
 
-  def maxDegreePlain(g: Graph[V, A, B]) = g.fold(0) { (c, z) =>
+  def maxDegreePlain(g: Graph[V, AA, BB]) = g.fold(0) { (c, z) =>
     g.decomp(c.vertex).toGDecomp.map {
       case GDecomp(Context(ins, _, _, outs), _) =>
         ins.size + outs.size
     }.getOrElse(0) max z
   }
 
-  def contextAsString[V, A, B](ctx: Option[Context[V, A, B]]): String = ctx match {
+  def contextOptionAsString[V, A, B](ctx: Option[Context[V, A, B]]): String = ctx match {
     case None       => "no decomposition"
-    case Some(ctx)  =>
-      s"""in      = ${ctx.inAdj}
-         |out     = ${ctx.outAdj}
-         |label   = ${ctx.label}
-         |vertex  = ${ctx.vertex}""".stripMargin
+    case Some(ctx)  => contextAsString(ctx)
+  }
+
+  def contextAsString[B, A, V](ctx: Context[V, A, B]) = {
+    s"""in      = ${ctx.inAdj}
+       |out     = ${ctx.outAdj}
+       |label   = ${ctx.label}
+       |vertex  = ${ctx.vertex}""".stripMargin
   }
 }

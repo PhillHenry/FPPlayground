8943bfed58bd3557457f7fe4b4cb9ecf274b6029
Phillip Henry <PhillHenry@gmail.com>
2021-02-01 11:33:14 +0000

    Implicits question.

---
+++ src/main/scala/uk/co/odinconsultants/fp/lang/Implicits.scala
@@ -0,0 +1,38 @@
+package uk.co.odinconsultants.fp.lang
+
+case class Limits[T: Numeric](x: T, y: T)
+
+abstract class RandomT[T: Numeric] {
+  def randomT(): T
+}
+
+object Implicits {
+
+  val rnd = new scala.util.Random
+
+  implicit class RandomInt(limits: Limits[Int]) extends RandomT[Int] {
+    def randomT(): Int = {
+      import limits._
+      val lower = math.min(x, y)
+      val upper = math.max(x, y)
+      val diff = upper - lower
+      rnd.nextInt(diff - 1) + lower
+    }
+  }
+
+}
+
+object ImplicitsMain {
+
+  import Implicits._
+
+  def main(args: Array[String]): Unit = {
+    ???
+  }
+
+  def assertEvenT[T](x: Limits[T])(implicit ev: RandomT[T]): T = {
+    implicit val gen = implicitly[RandomT[T]]
+//    x.randomT() // why does this not compile?
+    ???
+  }
+}

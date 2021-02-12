a8f60cce6418fc510e23062306c8229c7d886c68
Phillip Henry <PhillHenry@gmail.com>
2021-02-12 09:18:00 +0000

    warn-value-discard Seems to stop this pathological code.

--- src/main/scala/uk/co/odinconsultants/fp/cats/errors/GuaranteeMain.scala
+++ src/main/scala/uk/co/odinconsultants/fp/cats/errors/GuaranteeMain.scala
@@ -11,33 +11,47 @@ object GuaranteeMain extends IOApp {
     msg
   }
 
-  val io       = helloWorld.guarantee(IO {
-    println(aMessage)
-    aMessage
-  }) // this prints out the message
+  /**
+   * [ERROR] ... warning: discarded non-Unit value
+   */
+//  val io       = helloWorld.guarantee(IO {
+//    println(aMessage)
+//    aMessage
+//  }) // this prints out the message
+  /**
+   * [ERROR] ... warning: discarded non-Unit value
+   */
+//  val printingIO: IO[Unit] = IO {
+//    println(aMessage)
+//    aMessage
+//  }
+//  val io2 = helloWorld.guarantee(printingIO) // this prints out the message too
 
-  val printingIO: IO[Unit] = IO {
-    println(aMessage)
-    aMessage
-  }
-  val io2 = helloWorld.guarantee(printingIO) // this prints out the message too
+  /**
+   * [ERROR] ... warning: discarded non-Unit value
+   */
+//  def printingIOWith(x: String): IO[Unit] = IO {
+//    println(x)
+//    x
+//  }
+//  val io3 = helloWorld.guarantee(printingIOWith(aMessage)) // this prints out the message too
 
-  def printingIOWith(x: String): IO[Unit] = IO {
-    println(x)
+  def printingIOWithT[T](x: => T): IO[T] = IO {
+    println(s"IO passed '$x'")
     x
   }
-  val io3 = helloWorld.guarantee(printingIOWith(aMessage)) // this prints out the message too
-
-  def printingIOWithT[T](x: T): IO[T] = IO {
-    println(x)
-    x
-  }
-  val io4 = helloWorld.guarantee(printingIOWithT(aMessage))
-
-  val io5 = helloWorld.guarantee(IO {
-    println(aMessage)
-    aMessage
-  })
+  /**
+   * [ERROR] ... warning: discarded non-Unit value
+   */
+//  val io4 = helloWorld.guarantee(printingIOWithT("io4"))
+  /**
+   * [ERROR] ... warning: discarded non-Unit value
+   */
+//  val io5 = helloWorld.guarantee(IO {
+//    val msg = "io5"
+//    println(s"IO printing '$msg'")
+//    msg
+//  })
 
   def printingIOWithT2[T](x: T): IO[T] = {
     println(s"Creating IO[$x]")
@@ -46,8 +60,11 @@ object GuaranteeMain extends IOApp {
       x
     }
   }
-  val io6 = helloWorld.guarantee(printingIOWithT2(aMessage))
+  /**
+   * [ERROR] ... warning: discarded non-Unit value
+   */
+//  val io6 = helloWorld.guarantee(printingIOWithT2(aMessage))
 
   override def run(args: List[String]): IO[ExitCode] =
-    (io *> io2 *> io3 *> io4 *> io5 *> io6).as(ExitCode.Success)
+    (helloWorld /* *> io2 *> io3 *> io4 *> io5 *> io6 */).as(ExitCode.Success)
 }
